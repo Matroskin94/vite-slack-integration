f45e749ad18c12974525ad3d125703eb0ae9517a
Kotov Anton <antonkotov@MacBook-Pro-Anton.local>
2024-01-25 11:31:24 +0800

    [VITE-6] - Testing slack notification through action

---
+++ .github/workflows/develop-slack-notify.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MESSAGE_BODY='{
+          "blocks": [
+            {
+              "type": "section",
+              "block_id": "1",
+              "text": {
+                  "type": "mrkdwn",
+                  "text": "'"$SLACK_MESSAGE"'"
+              }
+            },
+            {
+              "type": "section",
+              "block_id": "1",
+              "text": {
+                  "type": "mrkdwn",
+                  "text": "'"$SLACK_TEST_MESSAGE"'"
+              }
+            }
+          ]
+        }'
+
+SLACK_URL=https://hooks.slack.com/services/$SLACK_WEBHOOK_SECRET
+
+curl -X POST -H 'Content-type: application/json' --data "$MESSAGE_BODY" "$SLACK_URL"
